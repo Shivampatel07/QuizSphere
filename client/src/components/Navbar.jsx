d43bcae425ac3cd551808d7f2a989b32b4a6117a
shivam <shivp72019@gmail.com>
2024-01-20 18:03:45 +0530

    css applied

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,22 +4,35 @@ import "./../css/navbar.css";
 function Navbar() {
   const [profileIcon, setProfileIcon] = useState(false);
   return (
-    <div className="navbar">
-      <div className="Logo">QuizSphere</div>
-      <div className="searchbar">
-        <input type="text" name="SearchBar" placeholder="search..." />
+    <>
+      <div></div>
+      <div className="navbar">
+        <div className="Logo">QuizSphere</div>
+        <div className="searchbar">
+          <input type="text" name="SearchBar" placeholder="search..." />
+        </div>
+        <div className="profile-icon">
+          <img
+            src="profile.png"
+            alt="Profile"
+            onClick={() => {
+              setProfileIcon(!profileIcon);
+              console.log(profileIcon);
+            }}
+          />
+        </div>
       </div>
-      <div className="profile-icon">
-        <img
-          src="profile.png"
-          alt="Profile"
-          onClick={() => {
-            setProfileIcon(!profileIcon);
-            console.log(profileIcon);
-          }}
-        />
+      <div className="sub-navbar">
+        <ul>
+          <li>Batch</li>
+          <li>Store</li>
+          <li>Subscription plan</li>
+          <li>GATE2024</li>
+          <li>JEE2024</li>
+          <li>Coding plateform</li>
+        </ul>
       </div>
-    </div>
+    </>
   );
 }
 
